435ef3a293619ea05f515d0eb13c37ef3f78f79d
Vadim Yalovets <vadim.yalovets@percona.com>
2022-07-14 11:44:10 +0300

    DISTMYSQL-197 version option does not return easily understandable version number

--- orchestrator_builder.sh
+++ orchestrator_builder.sh
@@ -112,7 +112,7 @@ get_sources(){
     cp ${WORKDIR}/orchestrator-packaging/percona-orchestrator.spec ./
     cd ../
     mv ${WORKDIR}/orchestrator-packaging/debian ./
-    ver=$(git rev-parse HEAD)
+    ver="${VERSION} ($(git rev-parse --short HEAD))"
     description=$(git describe --tags --always --dirty)
     sed -i "s:\$(git rev-parse HEAD):\"$ver\":" script/build
     sed -i "s:\$(git describe --tags --always --dirty):\"$description\":" script/build
