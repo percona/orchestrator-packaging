107e098974008efce2f5ebe5aecea4f0086208ad
Vadim Yalovets <vadim.yalovets@percona.com>
2025-07-24 22:29:40 +0300

    DISTMYSQL-536 Create new Orchestrator 3.2.6-18 packages

--- orchestrator_builder.sh
+++ orchestrator_builder.sh
@@ -158,13 +158,19 @@ get_system(){
 
 install_go() {
     if [ x"$ARCH" = "xx86_64" ]; then
-        wget https://go.dev/dl/go1.24.5.linux-amd64.tar.gz
+        until wget https://go.dev/dl/go1.24.5.linux-amd64.tar.gz; do
+            echo "Service not ready, retrying in 10 seconds..."
+            sleep 10
+        done
         rm -rf /usr/local/go
         tar -C /usr/local -xzf go1.24.5.linux-amd64.tar.gz
         update-alternatives --install /usr/bin/go go /usr/local/go/bin/go 1
         update-alternatives --set go /usr/local/go/bin/go
     else
-        wget https://go.dev/dl/go1.24.5.linux-arm64.tar.gz
+        until wget https://go.dev/dl/go1.24.5.linux-arm64.tar.gz; do
+            echo "Service not ready, retrying in 10 seconds..."
+            sleep 10
+        done
         rm -rf /usr/local/go
         tar -C /usr/local -xzf go1.24.5.linux-arm64.tar.gz
         update-alternatives --install /usr/bin/go go /usr/local/go/bin/go 1
